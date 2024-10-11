f964f120d17cb7c66934c047150ba5916c722c3c
Farhan Ishraq Omi <omifarhan@gmail.com>
2024-10-11 16:34:17 +0600

    fix port

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,7 @@
 // src/index.js
 import app from "./app.js";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
